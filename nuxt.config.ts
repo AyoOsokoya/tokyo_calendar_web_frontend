de32a96ca4dc5df17f376190ba5f39b6c528ef79
Ayo Osokoya <ayo.osokoya@gmail.com>
2023-07-22 20:34:30 +0900

    Install vuetify https://codybontecou.com/how-to-use-vuetify-with-nuxt-3.html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    css: ['bootstrap/dist/css/bootstrap.min.css'],
+    css: [
+        'vuetify/lib/styles/main.sass',
+        '@mdi/font/css/materialdesignicons.min.css',
+    ],
+    build: {
+        transpile: ['vuetify'],
+    },
     modules: [
         'nuxt-icon',
         'nuxt-lodash'
     ],
     buildModules: [
         '@nuxtjs/composition-api/module'
-    ]
+    ],
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+    },
 })
